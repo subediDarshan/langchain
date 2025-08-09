9dd3172a5b9f895ac11bf82965071a013f141145
Darshan Subedi <subedidarshan02@gmail.com>
2025-08-09 14:30:35 +0530

    tools complete

---
+++ 13_tools_in_langchain/1_into.py
@@ -0,0 +1,9 @@
+# a tool is python function or api that is packaged in such a way that LLM can understand and call it whenever needed.
+
+# Two types of tools:
+# 1. Built-in tools 
+# 2. Custom Tools
+
+
+# Built in tools:
+# (Tools that langchain already provides for you)
